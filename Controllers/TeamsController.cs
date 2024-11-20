b5b2742d81f49c9f221f6875a0ae173875c84509
Georgi Raev <georgihraev89@gmail.com>
2024-11-20 20:54:07 +0200

    added Team

---
+++ Controllers/TeamsController.cs
@@ -0,0 +1,39 @@
+﻿using F1_Web_App.Data;
+using F1_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace F1_Web_App.Controllers
+{
+    public class TeamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TeamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var model = await _context.Teams
+               .Include(t => t.Drivers)
+               .Select(t => new TeamListViewModel
+               {
+                   TeamName = t.Name,
+                   TeamImageUrl = t.ImageUrl,
+                   Drivers = t.Drivers.Select(d => new DriverListViewModel
+                   {
+                       Id = d.Id,
+                       DriverNumber = d.DriverNumber,
+                       Name = d.Name,
+                       TeamName = t.Name,
+                       ImageUrl = d.ImageUrl
+                   }).ToList()
+               })
+               .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/TeamListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using F1_Web_App.Data.Models;
+
+namespace F1_Web_App.Models
+{
+    public class TeamListViewModel
+    {
+        public string TeamName { get; set; } = null!;
+        public string TeamImageUrl { get; set; } = null!;
+        public ICollection<DriverListViewModel> Drivers { get; set; } = new List<DriverListViewModel>();
+
+    }
+}
