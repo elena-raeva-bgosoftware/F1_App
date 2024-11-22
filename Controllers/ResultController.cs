653703534fad63d717945437e40bc97c817a1f61
Georgi Raev <georgihraev89@gmail.com>
2024-11-22 17:55:48 +0200

    SqlResults

---
+++ Controllers/ResultController.cs
@@ -0,0 +1,36 @@
+﻿using F1_Web_App.Data;
+using F1_Web_App.Data.Models;
+using F1_Web_App.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace F1_Web_App.Controllers
+{
+    public class ResultController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ResultController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> IndexAsync(int EventId)
+        {
+            var model = await _context.Results
+                .Where(r => r.EventId == EventId)
+                .Select(r => new ResultViewModel
+                {
+                    Id = r.Id,
+                    DriverName = r.Driver.Name,
+                    DriverNumber = r.Driver.DriverNumber,
+                    TeamName = r.Driver.Team.Name,
+                    Points = r.Points
+                })
+                
+                .FirstOrDefaultAsync();
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/ResultViewModel.cs
@@ -0,0 +1,19 @@
+﻿using F1_Web_App.Data.Models;
+
+namespace F1_Web_App.Models
+{
+    public class ResultViewModel
+    {
+        public int Id { get; set; }
+
+        public string DriverName { get; set; }
+
+        public int DriverNumber { get; set; }
+
+        public string TeamName { get; set; }
+
+        public int Points { get; set; }
+
+        public Event Event { get; set; }
+    }
+}
